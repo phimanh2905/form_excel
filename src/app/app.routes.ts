544f1cf8899434345db1fbc1860a2e0bea2f197b
phmanh_thin <phmanh_thin@gmail.com>
2024-06-19 17:46:01 +0700

    them scroll ngang

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,15 +5,15 @@ import { GoodsQualityInspectionComponent } from './goods-quality-inspection/good
 import { ScrollGridComponent } from './scroll-grid/scroll-grid.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/address-form', pathMatch: 'full' },
+  { path: '', redirectTo: '/scroll', pathMatch: 'full' },
   { path: 'address-form', component: AddressFormComponent },
   { path: 'goods-form', component: GoodsFormComponent },
   {
     path: 'goods-quality-inspection',
     component: GoodsQualityInspectionComponent,
   },
-  {
-    path: 'scroll',
-    component: ScrollGridComponent,
-  },
+  // {
+  //   path: 'scroll',
+  //   component: ScrollGridComponent,
+  // },
 ];
--- src/app/scroll-grid/scroll-grid.component.ts
+++ src/app/scroll-grid/scroll-grid.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { fromEvent, filter } from 'rxjs';
 
 @Component({
   selector: 'app-scroll-grid',
   standalone: true,
   imports: [],
   templateUrl: './scroll-grid.component.html',
-  styleUrl: './scroll-grid.component.scss'
+  styleUrl: './scroll-grid.component.scss',
 })
 export class ScrollGridComponent {
-
+  ngOnInit(): void {
+    // const stickyElement = document.querySelector(
+    //   '.sticky-element'
+    // ) as HTMLElement;
+    // const scrollEvent$ = fromEvent(window, 'scroll');
+    // const scrolledToBottom$ = scrollEvent$
+    //   .pipe(
+    //     filter(() => {
+    //       const scrollTop =
+    //         window.scrollY || document.documentElement.scrollTop;
+    //       const height = document.documentElement.offsetHeight;
+    //       const windowHeight = window.innerHeight;
+    //       return scrollTop + windowHeight >= height;
+    //     })
+    //   )
+    //   .subscribe(() => {
+    //     console.log(`scrolledToBottom$`);
+    //   });
+  }
 }
