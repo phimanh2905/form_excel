631654b84424557a350ab292671af930895338f7
phmanh_m2 <phimanh2905@gmail.com>
2024-07-19 22:36:08 +0700

    scroll

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { GoodsQualityInspectionComponent } from './goods-quality-inspection/good
 import { ScrollGridComponent } from './scroll-grid/scroll-grid.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/address-form', pathMatch: 'full' },
+  { path: '', redirectTo: '/scroll', pathMatch: 'full' },
   { path: 'address-form', component: AddressFormComponent },
   { path: 'goods-form', component: GoodsFormComponent },
   {
